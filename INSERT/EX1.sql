389a9e8d93dae0ac77c0884c5e1915d097ca07f7
Harimaron <gaetan.ribonnet@outlook.com>
2017-11-09 09:17:12 +0100

    +insert

---
+++ INSERT/EX1.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Météo
+    (`ville` varchar(9), `haut` int, `bas` int)
+;
+    
+INSERT INTO Météo
+    (`ville`, `haut`, `bas`)
+VALUES
+    ('Bruxelles', 27, 13),
+    ('Liège', 25, 15),
+    ('Namur', 26, 15),
+    ('Charleroi', 25, 12),
+    ('Bruges', 28, 16)
+;
+
+INSERT INTO Météo
+    (ville, haut, bas)
+VALUES
+    ('Fukuoka', 23, 18)
+;
---
+++ SELECT/EX4.sql
@@ -0,0 +1,7 @@
+/*1*/SELECT COUNT('M') FROM octocats;
+/*2*/SELECT COUNT('F') FROM octocats WHERE promo='promo1-central';
+/*3*/SELECT COUNT('M') FROM octocats WHERE promo='promo1-central';
+/*4*/SELECT count(*) from octocats where firstname = 'Nadia';
+/*5*/SELECT ...
+/*6*/SELECT MAX(haut) FROM Météo;
+/*7*/SELECT MIN(bas) FROM Météo;
